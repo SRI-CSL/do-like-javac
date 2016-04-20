78f18d8486d303d5019b215463f33a7c0c187e70
Jianchu <jianchuli0624@gmail.com>
2016-04-20 17:09:59 -0400

    add option solverArgs

--- do_like_javac/tools/infer.py
+++ do_like_javac/tools/infer.py
@@ -18,28 +18,31 @@ infer_group.add_argument('-afud', '--afuOutputDir', metavar='<afud>',
 infer_group.add_argument('-m', '--mode', metavar='<mode>',
                         action='store',default='INFER',
                         help='Modes of operation: TYPECHECK, INFER, ROUNDTRIP,ROUNDTRIP_TYPECHECK')
+infer_group.add_argument('-solverArgs', '--solverArgs', metavar='<solverArgs>',
+                        action='store',default='backEndType=maxsatbackend.MaxSat',
+                        help='arguments for solver')
+
 
 def run(args, javac_commands, jars):
-    # the dist directory if CFI.
-    CFI_dist = os.environ['JSR308']+"/checker-framework-inference/dist"
-    CP_postfix = ":"+ CFI_dist + "/checker.jar:" + CFI_dist + "/plume.jar:" + \
-                 CFI_dist + "/checker-framework-inference.jar"
-
-
-    for jc in javac_commands:
-        pprint.pformat(jc)
-        javac_switches = jc['javac_switches']
-        target_cp = javac_switches['classpath']
-        java_files = ' '.join(jc['java_files'])
-
-        cp = target_cp + CP_postfix
-
-        cmd = ["java", "-classpath", cp, "checkers.inference.InferenceLauncher",
-               "--checker", args.checker, "--solver", args.solver,
-               "--mode" , args.mode ,"--targetclasspath", target_cp, "-afud", args.afuOutputDir, java_files]
-
-        print ("Running %s" % cmd)
-        try:
-            print (subprocess.check_output(cmd, stderr=subprocess.STDOUT))
-        except:
-            print ('calling {cmd} failed\n{trace}'.format(cmd=' '.join(cmd),trace=traceback.format_exc()))
+	# the dist directory if CFI.
+	CFI_dist = os.environ['JSR308']+"/checker-framework-inference/dist"
+	CFI_command = []
+	
+	CFI_command.extend(["java"])
+	
+	for jc in javac_commands:
+		pprint.pformat(jc)
+		javac_switches = jc['javac_switches']
+		target_cp = javac_switches['classpath']
+		cp = target_cp +":"+ CFI_dist + "/checker.jar:" + CFI_dist + "/plume.jar:" + \
+		     CFI_dist + "/checker-framework-inference.jar"
+		cmd = CFI_command + ["-classpath", cp, "checkers.inference.InferenceLauncher" , 
+				     "--solverArgs" ,args.solverArgs, "--checker" ,args.checker, "--solver", args.solver , 
+				     "--mode" , args.mode ,"--hacks=true","--targetclasspath", target_cp, "-afud", args.afuOutputDir]
+		for java_file in jc['java_files']:
+			cmd = cmd + [java_file]				     
+		print ("Running %s" % cmd)
+		try:
+			print (subprocess.check_output(cmd, stderr=subprocess.STDOUT))
+		except:
+			print ('calling {cmd} failed\n{trace}'.format(cmd=' '.join(cmd),trace=traceback.format_exc()))
