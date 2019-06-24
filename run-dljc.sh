ac5a359886ad060207a58322f998403243253f47
Michael Ernst <mernst@cs.washington.edu>
2019-06-24 16:29:15 -0700

    Documentation improvements

--- run-dljc.sh
+++ run-dljc.sh
@@ -1,49 +1,46 @@
 #!/bin/bash
 
-# usage:
+# This script runs the Checker Framework on each of a list of projects.
+
+### Usage
+
 # - Move this script to an experiment directory.
-# - Then create a list of git reposities, and place it in
-#   the file INLIST, one repository per line
-# - Then run the following command:
-#   > bash run-dljc.sh OUTDIR INLIST
+# - Make a file containing a list of git repositories, one per line.
+# - Set variables in the "Configuration" section below.
+# - Run the following command:
+#     bash run-dljc.sh OUTDIR INLIST
 #
 # Where OUTDIR is a name for the experiment you're running, and
-# INLIST is the list of git repositories.
+# INLIST is the file containing list of git repositories.
 #
-# This script will:
+# This script works as follows:
 # - Checkout each repository into OUTDIR/.
-# - Attempt to build it with do-like-javac.
-# - If it can be built, attempt to replay the javac commands
-#   with checkers enabled.
+# - Use do-like-javac to build it once, then
+#   replay the javac commands with checkers enabled.
 # - Place a copy of the results in the directory OUTDIR-results/.
-#
-# Configuration:
-# - Ensure that JAVA_HOME points to a java 8 JDK,
-#   or set the JAVA_HOME variable below.
-# - Configure the checkers to run using the CHECKERS variable below.
-# - Create a list of the dependencies of any custom checkers you
-#   want to use, and set the CHECKER_LIB variable below accordingly.
-# - Set the STUBS variable below if you want to use any stub files.
 
-# requirements:
-# JAVA_HOME must point to a Java 8 JDK for this script to work
+
+### Configuration
+
+# JAVA_HOME must point to a Java 8 JDK.
 if [ "x${JAVA_HOME}" = "x" ]; then
     JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
 fi
 
-# what typecheckers should be run?
+# What typecheckers should be run?
 CHECKERS=org.checkerframework.checker.builder.TypesafeBuilderChecker
 
-# if you want to use a custom typechecker or typecheckers,
-# define their dependencies here
+# If you want to use a custom typechecker or typecheckers,
+# define their dependencies here.
 CHECKER_LIB=/homes/gws/kelloggm/image-sniping-oss/typesafe-builder-checker/build/libs/typesafe-builder-checker.jar:/homes/gws/kelloggm/.m2/repository/org/springframework/spring-expression/5.1.7.RELEASE/spring-expression-5.1.7.RELEASE.jar:/homes/gws/kelloggm/.m2/repository/org/springframework/spring-core/5.1.7.RELEASE/spring-core-5.1.7.RELEASE.jar:/homes/gws/kelloggm/.m2/repository/org/springframework/spring-jcl/5.1.7.RELEASE/spring-jcl-5.1.7.RELEASE.jar:
 
-# should any stub files be used?
+# Should any stub files be used?
 STUBS=/homes/gws/kelloggm/image-sniping-oss/typesafe-builder-checker/stubs
 
-# script:
 
-# clone DLJC if its not present
+### Script
+
+# clone DLJC if it's not present
 if [ ! -d do-like-javac ]; then
     git clone https://github.com/kelloggm/do-like-javac
 fi
@@ -62,8 +59,8 @@ fi
     
 PATH=${JAVA_HOME}/bin:${PATH}
 
-mkdir $1 || true
-mkdir $1-results || true
+mkdir -p $1
+mkdir -p $1-results
 
 pushd $1
 
