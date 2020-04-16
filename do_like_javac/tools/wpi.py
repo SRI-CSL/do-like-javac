420da464abc353c39cf50ad3a28081501b71460a
Martin Kellogg <kelloggm@cs.washington.edu>
2020-04-16 13:14:39 -0700

    lift a bunch of computation out of the WPI loop, and do it once at the beginning. This avoids 1. delomboking every time, 2. recomputing the classpath, 3. recomputing the list of processors, and 4. recomputing the options that will stay constant each iteration

--- do_like_javac/tools/wpi.py
+++ do_like_javac/tools/wpi.py
@@ -48,6 +48,78 @@ def run(args, javac_commands, jars):
         iteration = 0
         diffResult = 1
         stubDirs = []
+
+        javac_switches = jc['javac_switches']
+        cp = javac_switches['classpath']
+        if javac_switches.has_key('processor') and len(processorArg) == 2:
+            processorArg[1] += "," + javac_switches['processor']
+
+        java_files = jc['java_files']
+
+        # delombok
+        jars = cp.split(":")
+        lombokjar = ""
+        for jar in jars:
+            # This should catch only the Lombok jar, because it's based
+            # on Lombok's Maven coordinates. First is the Maven repo file structure;
+            # second is the gradle cache's file structure.
+            if "/org/projectlombok/lombok/" in jar or "/org.projectlombok/lombok/":
+                lombokjar = jar
+
+        # must wait until here to supply the classpath without lombok
+        if lombokjar != "":
+            # delombok takes a directory as input rather than individual source files,
+            # so this guesses at what the correct top-level directory is. It's a hack,
+            # but it should work for Maven and Gradle projects that follow the default
+            # conventions. For compilation to make sense, there must be at least one
+            # Java file, so this access should be safe.
+            anySrcFile = java_files[0]
+            standardSrcDir = "src/main/java/"
+
+            standardSrcIndex = anySrcFile.index(standardSrcDir)
+
+            if standardSrcDir != -1:
+                srcDir = anySrcFile[:standardSrcIndex]
+                lombok_cmd = ["java", "-jar", lombokjar, "delombok",
+                              srcDir + "/src/main/java/", "-d", srcDir + "/delombok/main/java",
+                              "-c", cp]
+                common.run_cmd(lombok_cmd, args, "wpi")
+                # replace the original source files with the delombok'd code, so that
+                # the actual javac commands don't need to be modified
+                dir_util.copy_tree(srcDir + "/delombok/", srcDir + "/src/")
+
+                # suppress all type.anno.before.modifier warnings, because delombok
+                # prints annotations in the wrong place
+                iterationCheckerCmd.append("-AsuppressWarnings=type.anno.before.modifier")
+
+        # include processor path in the class path if it is present
+        pp = ''
+        if javac_switches.has_key('processorpath'):
+            pp = javac_switches['processorpath'] + ':'
+        if args.quals:
+            cp += args.quals + ':'
+        if args.lib_dir:
+            cp += pp + args.lib_dir + ':'
+
+        other_args = []
+        for k, v in javac_switches.items():
+            if k not in banned_options:
+                if k == "source" or k == "target":
+                    # if the source/target is < 8, change it to 8
+                    if v in ["1.5", "5", "1.6", "6", "1.7", "7"]:
+                        v = "8"
+                    if v == "1.8":
+                        v = "8"
+                    # Do not use source/target, because Java 11 JVMs will
+                    # crash on some classes, e.g.
+                    # https://bugs.openjdk.java.net/browse/JDK-8212636.
+                    # Use --release instead.
+                    k = "-release"
+                if v is None or v is not False:
+                    other_args.append("-" + k)
+                if v is not None and v is not True:
+                    other_args.append(str(v))
+
         while diffResult != 0:
 
             iterationStubs = ':'.join(stubDirs)
@@ -64,76 +136,7 @@ def run(args, javac_commands, jars):
                 iterationCheckerCmd = checker_command
 
             pprint.pformat(jc)
-            javac_switches = jc['javac_switches']
-            cp = javac_switches['classpath']
-            if javac_switches.has_key('processor') and len(processorArg) == 2:
-                processorArg[1] += "," + javac_switches['processor']
 
-            java_files = jc['java_files']
-
-            # delombok
-            jars = cp.split(":")
-            lombokjar = ""
-            for jar in jars:
-                # This should catch only the Lombok jar, because it's based
-                # on Lombok's Maven coordinates. First is the Maven repo file structure;
-                # second is the gradle cache's file structure.
-                if "/org/projectlombok/lombok/" in jar or "/org.projectlombok/lombok/":
-                    lombokjar = jar
-
-            # must wait until here to supply the classpath without lombok
-            if lombokjar != "":
-                # delombok takes a directory as input rather than individual source files,
-                # so this guesses at what the correct top-level directory is. It's a hack,
-                # but it should work for Maven and Gradle projects that follow the default
-                # conventions. For compilation to make sense, there must be at least one
-                # Java file, so this access should be safe.
-                anySrcFile = java_files[0]
-                standardSrcDir = "src/main/java/"
-
-                standardSrcIndex = anySrcFile.index(standardSrcDir)
-
-                if standardSrcDir != -1:
-                    srcDir = anySrcFile[:standardSrcIndex]
-                    lombok_cmd = ["java", "-jar", lombokjar, "delombok",
-                                  srcDir + "/src/main/java/", "-d", srcDir + "/delombok/main/java",
-                                  "-c", cp]
-                    common.run_cmd(lombok_cmd, args, "wpi")
-                    # replace the original source files with the delombok'd code, so that
-                    # the actual javac commands don't need to be modified
-                    dir_util.copy_tree(srcDir + "/delombok/", srcDir + "/src/")
-
-                    # suppress all type.anno.before.modifier warnings, because delombok
-                    # prints annotations in the wrong place
-                    iterationCheckerCmd.append("-AsuppressWarnings=type.anno.before.modifier")
-
-            # include processor path in the class path if it is present
-            pp = ''
-            if javac_switches.has_key('processorpath'):
-                pp = javac_switches['processorpath'] + ':'
-            if args.quals:
-                 cp += args.quals + ':'
-            if args.lib_dir:
-                cp += pp + args.lib_dir + ':'
-
-            other_args = []
-            for k, v in javac_switches.items():
-                if k not in banned_options:
-                    if k == "source" or k == "target":
-                        # if the source/target is < 8, change it to 8
-                        if v in ["1.5", "5", "1.6", "6", "1.7", "7"]:
-                            v = "8"
-                        if v == "1.8":
-                            v = "8"
-                        # Do not use source/target, because Java 11 JVMs will
-                        # crash on some classes, e.g.
-                        # https://bugs.openjdk.java.net/browse/JDK-8212636.
-                        # Use --release instead.
-                        k = "-release"
-                    if v is None or v is not False:
-                        other_args.append("-" + k)
-                    if v is not None and v is not True:
-                        other_args.append(str(v))
             cmd = iterationCheckerCmd + ["-classpath", cp] + processorArg + other_args + java_files
             stats = common.run_cmd(cmd, args, 'wpi')
 
