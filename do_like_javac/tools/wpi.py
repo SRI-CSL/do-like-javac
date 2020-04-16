c7058bc660957237f3e7fb0a823f01b2a4d10004
Martin Kellogg <kelloggm@cs.washington.edu>
2020-04-15 17:29:42 -0700

    correct data structure of command

--- do_like_javac/tools/wpi.py
+++ do_like_javac/tools/wpi.py
@@ -106,9 +106,10 @@ def run(args, javac_commands, jars):
 
                 if standardSrcDir != -1:
                     srcDir = anySrcFile[:standardSrcIndex]
-                    lombok_cmd = "java -jar " + lombokjar + " delombok " + srcDir \
-                                 + "/src/main/java/ -d " + srcDir + "/delombok/main/java -c " + new_cp
-                    common.run_cmd([lombok_cmd], args, "wpi")
+                    lombok_cmd = ["java", "-jar", lombokjar, "delombok",
+                                  srcDir + "/src/main/java/", "-d", srcDir + "/delombok/main/java",
+                                  "-c", new_cp]
+                    common.run_cmd(lombok_cmd, args, "wpi")
                     # replace the original source files with the delombok'd code, so that
                     # the actual javac commands don't need to be modified
                     dir_util.copy_tree(srcDir + "/delombok/", srcDir + "/src/")
