6d3fd7ee57ae94d0eefd2830da1564903dfefdfe
Michael Ernst <mernst@alum.mit.edu>
2021-04-01 11:43:12 -0700

    Add date to temporary directory name (#33)

--- do_like_javac/tools/wpi.py
+++ do_like_javac/tools/wpi.py
@@ -1,6 +1,7 @@
 from filecmp import dircmp
 
 import common
+import datetime
 import os
 import pprint
 import shutil
@@ -54,7 +55,7 @@ def run(args, javac_commands, jars):
         iteration = 0
         diffResult = 1
         stubDirs = []
-        resultsDir = tempfile.mkdtemp(prefix="wpi-stubs-")
+        resultsDir = tempfile.mkdtemp(prefix="wpi-stubs-" + datetime.now().format("%Y%m%d%H%M%S)"))
 
         print "Directory for generated stub files: " + str(resultsDir)
 
