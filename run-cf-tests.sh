8b0dd96ab350e192442a4926c5468de3648b29fa
Martin Kellogg <kelloggm@cs.washington.edu>
2021-05-18 00:30:47 +0000

    don't rely on git-clone-related for the version of DLJC that's under test (#40)

--- run-cf-tests.sh
+++ run-cf-tests.sh
@@ -22,18 +22,9 @@ rm -rf /tmp/"$USER"/checker-framework
 /tmp/"$USER"/plume-scripts/git-clone-related typetools checker-framework /tmp/"$USER"/checker-framework
 export CHECKERFRAMEWORK=/tmp/"$USER"/checker-framework
 
-# see https://stackoverflow.com/questions/58033366/how-to-get-current-branch-within-github-actions/58035262
-dljc_branch_name=${GITHUB_REF##*/}
-
-### enforce that the CF is on the same branch that this copy of DLJC is - if git-clone-related's fallback
-### branch (i.e. master for the CF) is being used, we need to switch to a new branch with this branch's name,
-### so that uses of git-clone-related from the CF's own scripts to fetch do-like-javac return the current branch
+export DLJC=$(pwd)/dljc
 
 cd /tmp/"$USER"/checker-framework
-cf_branch_name=$(git rev-parse --abbrev-ref HEAD)
-if [ ! "${dljc_branch_name}" = "${cf_branch_name}" ]; then
-  git checkout -b "${dljc_branch_name}"
-fi
 
 ### run the CF tests
 ./gradlew wpiManyTests wpiPlumeLibTests
